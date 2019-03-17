1ea325a04bd702293e514e7f53309d2b544ed666
Conrad Kang <43460783+MrChopPot@users.noreply.github.com>
2019-03-17 14:58:00 -0400

    Update RecSys.py

--- RecSys.py
+++ RecSys.py
@@ -5,3 +5,16 @@ TJ_ratings.show()
 
 # Generate recommendations for users
 get_ALS_recs(['Jane', 'Taylor'])
+
+# Group the data by "Genre"
+markus_ratings.groupBy("Genre").sum().show()
+
+# Examine matrix P using the .show() method
+P.show()
+
+# Examine matrix Pi using the .show() method
+Pi.show()
+
+##########################
+
+### 2. How does ALS work?
