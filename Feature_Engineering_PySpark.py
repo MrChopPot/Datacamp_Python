4cb3d2492d3a67301f440a4cc17a573529054766
Conrad Kang <43460783+MrChopPot@users.noreply.github.com>
2019-03-21 15:59:17 -0400

    Create Feature_Engineering_PySpark.py

---
+++ Feature_Engineering_PySpark.py
@@ -0,0 +1,92 @@
+### Feature Engineering with PySpark
+
+### 1. Exploratory Data Analysis
+
+# Return spark version
+print(spark.version)
+
+# Return python version
+import sys
+print(sys.version_info)
+
+# Read the file into a dataframe
+df = spark.read.parquet('Real_Estate.parq')
+# Print columns in dataframe
+print(df.columns)
+
+# Select our dependent variable
+Y_df = df.select(["SALESCLOSEPRICE"])
+
+# Display summary statistics
+Y_df.describe().show()
+
+def check_load(df, num_records, num_columns):
+  # Takes a dataframe and compares record and column counts to input
+  # Message to return if the critera below aren't met
+  message = 'Validation Failed'
+  # Check number of records
+  if num_records == df.count():
+    # Check number of columns
+    if num_columns == len(df.columns):
+      # Success message
+      message = 'Validation Passed'
+  return message
+
+# Print the data validation message
+print(check_load(df, 5000, 74))
+
+# create list of actual dtypes to check
+actual_dtypes_list = df.dtypes
+print(actual_dtypes_list)
+
+# Iterate through the list of actual dtypes tuples
+for attribute_tuple in actual_dtypes_list:
+  
+  # Check if column name is dictionary of expected dtypes
+  col_name = attribute_tuple[0]
+  if col_name in validation_dict:
+
+    # Compare attribute types
+    col_type = attribute_tuple[1]
+    if col_type == validation_dict[col_name]:
+      print(col_name + ' has expected dtype.')
+
+# Name and value of col with max corr
+corr_max = 0
+corr_max_col = columns[0]
+
+# Loop to check all columns contained in list
+for col in columns:
+    # Check the correlation of a pair of columns
+    corr_val = df.corr(col, 'SALESCLOSEPRICE')
+    # Logic to compare corr_max with current corr_val
+    if corr_val > corr_max:
+        # Update the column name and corr value
+        corr_max = corr_val
+        corr_max_col = col
+
+print(corr_max_col)
+
+# Select a single column and sample and convert to pandas
+sample_df = df.select(['LISTPRICE']).sample(False, .5, 42)
+pandas_df = sample_df.toPandas()
+
+# Plot distribution of pandas_df and display plot
+sns.distplot(pandas_df)
+plt.show()
+
+# Import skewness function
+from pyspark.sql.functions import skewness
+
+# Compute and print skewness of LISTPRICE
+print(df.agg({'LISTPRICE': 'skewness'}).collect())
+
+# Select a the relevant columns and sample
+sample_df = df.select(['SALESCLOSEPRICE', 'LIVINGAREA' ]).sample(False, .5, 42)
+
+# Convert to pandas dataframe
+pandas_df = sample_df.toPandas()
+
+# Linear model plot of pandas_df
+sns.lmplot(x='LIVINGAREA', y='SALESCLOSEPRICE', data=pandas_df)
+plt.show()
