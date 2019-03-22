9082f6f7cf20d48af4b662198653745647074764
Conrad Kang <43460783+MrChopPot@users.noreply.github.com>
2019-03-22 13:34:24 -0400

    Update Preprocessing_for_ML.py

--- Preprocessing_for_ML.py
+++ Preprocessing_for_ML.py
@@ -36,3 +36,104 @@ X_train, X_test, y_train, y_test = train_test_split(volunteer_X, volunteer_y, st
 
 # Print out the category_desc counts on the training y labels
 print(y_train["category_desc"].value_counts())
+
+#######################
+
+### 2. Standardizing Data
+
+# Split the dataset and labels into training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y)
+
+# Fit the k-nearest neighbors model to the training data
+knn.fit(X_train, y_train)
+
+# Score the model on the test data
+print(knn.score(X_test, y_test))
+
+# Log normalization applies to high variance variables
+wine.Proline.var()
+
+# Print out the variance of the Proline column
+print(wine.Proline.var())
+
+# Apply the log normalization function to the Proline column
+wine["Proline_log"] = np.log(wine.Proline)
+
+# Check the variance of the Proline column again
+print(wine["Proline_log"].var())
+
+# Import StandardScaler from scikit-learn
+from sklearn.preprocessing import StandardScaler
+
+# Create the scaler
+ss = StandardScaler()
+
+# Take a subset of the DataFrame you want to scale 
+wine_subset = wine[["Ash", "Alcalinity of ash", "Magnesium"]]
+
+# Apply the scaler to the DataFrame subset
+wine_subset_scaled = ss.fit_transform(wine_subset)
+
+# Split the dataset and labels into training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y)
+
+# Fit the k-nearest neighbors model to the training data
+knn.fit(X_train, y_train)
+
+# Score the model on the test data (score: R^2 for Reg, accuracy for Cla)
+print(knn.score(X_test, y_test))
+
+# Create the scaling method.
+ss = StandardScaler()
+
+# Apply the scaling method to the dataset used for modeling.
+X_scaled = ss.fit_transform(X)
+X_train, X_test, y_train, y_test = train_test_split(X_scaled, y)
+
+# Fit the k-nearest neighbors model to the training data.
+knn.fit(X_train, y_train)
+
+# Score the model on the test data.
+print(knn.score(X_test, y_test))
+
+#######################
+
+### 3. Feature Engineering
+
+# Set up the LabelEncoder object
+enc = LabelEncoder()
+
+# Apply the encoding to the "Accessible" column
+hiking["Accessible_enc"] = enc.fit_transform(hiking["Accessible"])
+
+# Compare the two columns
+print(hiking[["Accessible", "Accessible_enc"]].head())
+
+# Transform the category_desc column
+category_enc = pd.get_dummies(volunteer["category_desc"])
+
+# Take a look at the encoded columns
+print(category_enc.head())
+
+# Create a list of the columns to average
+run_columns = ["run1", "run2", "run3", "run4", "run5"]
+
+# Use apply to create a mean column
+running_times_5k["mean"] = running_times_5k.apply(lambda row: row[run_columns].mean(), axis=1)
+
+# Take a look at the results
+print(running_times_5k)
+
+# First, convert string column to date column
+volunteer["start_date_converted"] = pd.to_datetime(volunteer["start_date_date"])
+
+# Extract just the month from the converted column
+volunteer["start_date_month"] = volunteer["start_date_converted"].apply(lambda row: row.month)
+
+# Take a look at the original and new columns
+print(volunteer[["start_date_month", "start_date_converted"]].head)
+
+
+
+
+
