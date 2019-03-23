25b1a7c2c185255676dff2ab44b1d345642ccdbe
Conrad Kang <43460783+MrChopPot@users.noreply.github.com>
2019-03-22 22:27:01 -0400

    Update Preprocessing_for_ML.py

--- Preprocessing_for_ML.py
+++ Preprocessing_for_ML.py
@@ -133,7 +133,119 @@ volunteer["start_date_month"] = volunteer["start_date_converted"].apply(lambda r
 # Take a look at the original and new columns
 print(volunteer[["start_date_month", "start_date_converted"]].head)
 
+# Write a pattern to extract numbers and decimals
+def return_mileage(length):
+    pattern = re.compile(r"\d+\.\d+")
+    
+    # Search the text for matches
+    mile = re.match(pattern, length)
+    
+    # If a value is returned, use group(0) to return the found value
+    if mile is not None:
+        return float(mile.group(0))
+        
+# Apply the function to the Length column and take a look at both columns
+hiking["Length_num"] = hiking["Length"].apply(lambda row: return_mileage(row))
+print(hiking[["Length", "Length_num"]].head())
 
+# Take the title text
+title_text = volunteer["title"]
 
+# Create the vectorizer method
+tfidf_vec = TfidfVectorizer()
 
+# Transform the text into tf-idf vectors
+text_tfidf = tfidf_vec.fit_transform(title_text)
+
+# Split the dataset according to the class distribution of category_desc
+y = volunteer["category_desc"]
+train_X, test_X, train_y, test_y = train_test_split(text_tfidf.toarray(), y, stratify=y)
+
+# Fit the model to the training data
+nb.fit(train_X, train_y)
+
+# Print out the model's accuracy
+print(nb.score(test_X, test_y))
+
+#######################
+
+### 4. Selecting features for modeling
+
+# Create a list of redundant column names to drop
+to_drop = ["category_desc", "created_date", "locality", "region", "vol_requests"]
+
+# Drop those columns from the dataset
+volunteer_subset = volunteer.drop(to_drop, axis=1)
+
+# Print out the head of the new dataset
+print(volunteer_subset.head())
+
+# Print out the column correlations of the wine dataset
+print(wine.corr())
+
+# Take a minute to find the column where the correlation value is greater than 0.75 at least twice
+to_drop = "Flavanoids"
+
+# Drop that column from the DataFrame
+wine = wine.drop(to_drop, axis=1)
+
+# Add in the rest of the parameters
+def return_weights(vocab, original_vocab, vector, vector_index, top_n):
+    zipped = dict(zip(vector[vector_index].indices, vector[vector_index].data))
+    
+    # Let's transform that zipped dict into a series
+    zipped_series = pd.Series({vocab[i]:zipped[i] for i in vector[vector_index].indices})
+    
+    # Let's sort the series to pull out the top n weighted words
+    zipped_index = zipped_series.sort_values(ascending=False)[:top_n].index
+    return [original_vocab[i] for i in zipped_index]
+
+# Print out the weighted words
+print(return_weights(vocab, tfidf_vec.vocabulary_, text_tfidf, 8, 3))
+
+def words_to_filter(vocab, original_vocab, vector, top_n):
+    filter_list = []
+    for i in range(0, vector.shape[0]):
+    
+        # Here we'll call the function from the previous exercise, and extend the list we're creating
+        filtered = return_weights(vocab, original_vocab, vector, i, top_n)
+        filter_list.extend(filtered)
+    # Return the list in a set, so we don't get duplicate word indices
+    return set(filter_list)
+
+# Call the function to get the list of word indices
+filtered_words = words_to_filter(vocab, tfidf_vec.vocabulary_, text_tfidf, 3)
+
+# By converting filtered_words back to a list, we can use it to filter the columns in the text vector
+filtered_text = text_tfidf[:, list(filtered_words)]
+
+# Split the dataset according to the class distribution of category_desc, using the filtered_text vector
+train_X, test_X, train_y, test_y = train_test_split(filtered_text.toarray(), volunteer.category_desc, stratify=volunteer.category_desc)
+
+# Fit the model to the training data
+nb.fit(train_X, train_y)
+
+# Print out the model's accuracy
+print(nb.score(test_X, test_y))
+
+from sklearn.decomposition import PCA
+
+# Set up PCA and the X vector for diminsionality reduction
+pca = PCA()
+wine_X = wine.drop("Type", axis=1)
+
+# Apply PCA to the wine dataset X vector
+transformed_X = pca.fit_transform(wine_X)
+
+# Look at the percentage of variance explained by the different components
+print(pca.explained_variance_ratio_)
+
+# Split the transformed X and the y labels into training and test sets
+X_wine_train, X_wine_test, y_wine_train, y_wine_test = train_test_split(transformed_X, y)
+
+# Fit knn to the training data
+knn.fit(X_wine_train, y_wine_train)
+
+# Score knn on the test data and print it out
+knn.score(X_wine_test, y_wine_test)
 
