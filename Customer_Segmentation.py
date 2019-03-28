b058b951e50398a41a5e8f25fcaeae87961b6090
Conrad Kang <43460783+MrChopPot@users.noreply.github.com>
2019-03-28 00:05:59 -0400

    Create Customer_Segmentation.py

---
+++ Customer_Segmentation.py
@@ -0,0 +1,91 @@
+### Customer Segmentation in Python
+
+### 1. Cohort Analysis
+
+# Define a function that will parse the date
+def get_day(x): return dt.datetime(x.year, x.month, x.day) 
+
+# Create InvoiceDay column
+online['InvoiceDay'] = online['InvoiceDate'].apply(get_day) 
+
+# Group by CustomerID and select the InvoiceDay value
+grouping = online.groupby('CustomerID')['InvoiceDay'] 
+
+# Assign a minimum InvoiceDay value to the dataset
+online['CohortDay'] = grouping.transform('min')
+
+# View the top 5 rows
+print(online.head())
+
+# Get the integers for date parts from the `InvoiceDay` column
+invoice_year, invoice_month, invoice_day = get_date_int(online, "InvoiceDay")
+
+# Get the integers for date parts from the `CohortDay` column
+cohort_year, cohort_month, cohort_day = get_date_int(online, "CohortDay")
+
+# Calculate difference in years
+years_diff = invoice_year - cohort_year
+
+# Calculate difference in months
+months_diff = invoice_month - cohort_month
+
+# Calculate difference in days
+days_diff = invoice_day - cohort_day
+
+# Extract the difference in days from all previous values
+online['CohortIndex'] = years_diff * 365 + months_diff * 30 + days_diff + 1
+print(online.head())
+
+# Count the number of unique values per customer ID
+cohort_data = grouping["CustomerID"].apply(pd.Series.nunique).reset_index()
+
+# Create a pivot 
+cohort_counts = cohort_data.pivot(index="CohortMonth", columns="CohortIndex", values="CustomerID")
+
+# Select the first column and store it to cohort_sizes
+cohort_sizes = cohort_counts.iloc[:,0]
+
+# Divide the cohort count by cohort sizes along the rows
+retention = cohort_counts.divide(cohort_sizes, axis=0)
+
+# Create a groupby object and pass the monthly cohort and cohort index as a list
+grouping = online.groupby(["CohortMonth", "CohortIndex"]) 
+
+# Calculate the average of the unit price column
+cohort_data = grouping["UnitPrice"].mean()
+
+# Reset the index of cohort_data
+cohort_data = cohort_data.reset_index()
+
+# Create a pivot 
+average_quantity = cohort_data.pivot(index="CohortMonth", columns="CohortIndex", values="UnitPrice")
+print(average_quantity.round(1))
+
+# Import seaborn package as sns
+import seaborn as sns
+
+# Initialize an 8 by 6 inches plot figure
+plt.figure(figsize=(8, 6))
+
+# Add a title
+plt.title('Average Spend by Monthly Cohorts')
+
+# Create the heatmap
+sns.heatmap(average_quantity, annot=True, cmap='Blues')
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
