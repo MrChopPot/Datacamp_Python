024796e6fd08c9e84dd7c042c6cb3c1f9fe39c00
Conrad Kang <43460783+MrChopPot@users.noreply.github.com>
2019-04-19 00:45:15 -0400

    Create Network_Analysis.py

---
+++ Network_Analysis.py
@@ -0,0 +1,119 @@
+### Network Analysis in Python (Part 1)
+
+### 1. Introduction to networks
+
+len(T)
+
+type(T.nodes())
+
+type(list(T.edges(data=True))[-1][2])
+
+# Import necessary modules
+import matplotlib.pyplot as plt
+import networkx as nx
+
+# Draw the graph to screen
+nx.draw(T_sub)
+plt.show()
+
+# Use a list comprehension to get the nodes of interest: noi
+noi = [n for n, d in T.nodes(data = True) if d['occupation'] == 'scientist']
+
+# Use a list comprehension to get the edges of interest: eoi
+eoi = [(u, v) for u, v, d in T.edges(data=True) if d['date'] < date(2010, 1, 1)]
+
+# Set the weight of the edge
+T.edges[1, 10]['weight'] = 2
+
+# Iterate over all the edges (with metadata)
+for u, v, d in T.edges(data = True):
+
+    # Check if node 293 is involved
+    if 293 in [u, v]:
+
+        # Set the weight to 1.1
+        T.edges[u, v]['weight'] = 1.1
+
+# Define find_selfloop_nodes()
+def find_selfloop_nodes(G):
+    """
+    Finds all nodes that have self-loops in the graph G.
+    """
+    nodes_in_selfloops = []
+
+    # Iterate over all the edges of G
+    for u, v in G.edges(data = False):
+
+    # Check if node u and node v are the same
+        if u == v:
+
+            # Append node u to nodes_in_selfloops
+            nodes_in_selfloops.append(u)
+
+    return nodes_in_selfloops
+
+# Check whether number of self loops equals the number of nodes in self loops
+assert T.number_of_selfloops() == len(find_selfloop_nodes(T))
+
+# Import nxviz
+import nxviz
+
+# Create the MatrixPlot object: m
+m = nxviz.MatrixPlot(T)
+
+# Draw m to the screen
+m.draw()
+
+# Display the plot
+plt.show()
+
+# Convert T to a matrix format: A
+A = nx.to_numpy_matrix(T)
+
+# Convert A back to the NetworkX form as a directed graph: T_conv
+T_conv = nx.from_numpy_matrix(A, create_using=nx.DiGraph())
+
+# Check that the `category` metadata field is lost from each node
+for n, d in T_conv.nodes(data=True):
+    assert 'category' not in d.keys()
+
+# Import necessary modules
+import matplotlib.pyplot as plt
+from nxviz import CircosPlot
+
+# Create the CircosPlot object: c
+c = CircosPlot(T)
+
+# Draw c to the screen
+c.draw()
+
+# Display the plot
+plt.show()
+
+# Import necessary modules
+import matplotlib.pyplot as plt
+from nxviz import ArcPlot
+
+# Create the un-customized ArcPlot object: a
+a = ArcPlot(T)
+
+# Draw a to the screen
+a.draw()
+
+# Display the plot
+plt.show()
+
+# Create the customized ArcPlot object: a2
+a2 = ArcPlot(T, node_order = 'category', node_color = 'category', )
+
+# Draw a2 to the screen
+a2.draw()
+
+# Display the plot
+plt.show()
+
+#######################
+
+### 2. Important nodes
+
+
