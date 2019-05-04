1f29031fa80f6d0023cfd2c7f0e1e5339cac9e69
Conrad Kang <43460783+MrChopPot@users.noreply.github.com>
2019-05-04 15:31:14 -0400

    Update Network_Analysis.py

--- Network_Analysis.py
+++ Network_Analysis.py
@@ -116,4 +116,75 @@ plt.show()
 
 ### 2. Important nodes
 
+# Define nodes_with_m_nbrs()
+def nodes_with_m_nbrs(G, m):
+    """
+    Returns all nodes in graph G that have m neighbors.
+    """
+    nodes = set()
+
+    # Iterate over all nodes in G
+    for n in G.nodes():
+
+        # Check if the number of neighbors of n matches m
+        if len(list(G.neighbors(n))) == m:
+
+            # Add the node n to the set
+            nodes.add(n)
+
+    # Return the nodes with m neighbors
+    return nodes
+
+# Compute and print all nodes in T that have 6 neighbors
+six_nbrs = nodes_with_m_nbrs(T, 6)
+print(six_nbrs)
+
+# Compute the degree of every node: degrees
+degrees = [len(list(T.neighbors(n))) for n in T.nodes()]
+
+# Print the degrees
+print(degrees)
+
+# Import matplotlib.pyplot
+import matplotlib.pyplot as plt
+
+# Compute the degree centrality of the Twitter network: deg_cent
+deg_cent = nx.degree_centrality(T)
+
+# Plot a histogram of the degree centrality distribution of the graph.
+plt.figure()
+plt.hist(list(deg_cent.values()))
+plt.show()
+
+# Plot a histogram of the degree distribution of the graph
+plt.figure()
+plt.hist([len(list(T.neighbors(n))) for n in T.nodes()])
+plt.show()
+
+# Plot a scatter plot of the centrality distribution and the degree distribution
+plt.figure()
+plt.scatter(degrees, list(deg_cent.values()))
+plt.show()
+
+# Define path_exists()
+def path_exists(G, node1, node2):
+    """
+    This function checks whether a path exists between two nodes (node1, node2) in graph G.
+    """
+    visited_nodes = set()
+
+    # Initialize the queue of cells to visit with the first node: queue
+    queue = [node1]
+
+    # Iterate over the nodes in the queue
+    for node in queue:
+
+        # Get neighbors of the node
+        neighbors = G.neighbors(node)
+
+        # Check to see if the destination node is in the set of neighbors
+        if node2 in neighbors:
+            print('Path exists between nodes {0} and {1}'.format(node1, node2))
+            return True
+            break
 
