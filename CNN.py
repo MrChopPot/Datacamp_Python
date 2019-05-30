e6c7004b2cfc88bab5421167546ec75ce1d3a11e
Conrad Kang <43460783+MrChopPot@users.noreply.github.com>
2019-05-29 21:07:43 -0400

    Update CNN.py

--- CNN.py
+++ CNN.py
@@ -86,3 +86,25 @@ model.evaluate(test_data, test_labels)
 
 ### 2. Using Convolutions
 
+array = np.array([1, 0, 1, 0, 1, 0, 1, 0, 1, 0])
+kernel = np.array([1, -1, 0])
+conv = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+
+# Output array
+for ii in range(8):
+    conv[ii] = (kernel * array[ii:ii+3]).sum()
+
+# Print conv
+print(conv)
+
+kernel = np.array([[0, 1, 0], [1, 1, 1], [0, 1, 0]])
+result = np.zeros(im.shape)
+
+# Output array
+for ii in range(im.shape[0] - 3):
+    for jj in range(im.shape[1] - 3):
+        result[ii, jj] = (im[ii:ii+3, jj:jj+3] * kernel).sum()
+
+# Print result
+print(result)
+
